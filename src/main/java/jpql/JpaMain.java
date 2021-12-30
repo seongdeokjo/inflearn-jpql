7070ad3d6044cca301d12d23a89ac49b32aa7b0a
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-12-30 11:06:46 +0900

    feat: inflearn - jpa - jpql - 객체지향 쿼리 언어1 - 기본 문법 - 페치 조인 기본 + 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -11,17 +11,36 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-            member.setTeam(team);
-            em.persist(member);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
             em.flush();
             em.clear();
+//          --------------------------------------------------------------------
+            // fetch join
+//            String query = "select m from Member m";
+//            String query = "select m from Member m join fetch m.team";
+//            List<Member> result = em.createQuery(query, Member.class).getResultList();
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername()+", "+member.getTeam().getName());
+//                // 회원1, 팀A(sql) / 회원2, 팀A(1차캐시) / 회원3, 팀B(sql)
+//                // 회원 100명 -> N + 1
+//            }
 //          --------------------------------------------------------------------
             // nullif : 두 값이 같으면 null 반환, 다르면 첫번째 값 반환
 //            String query = "select nullif(m.username,'관리자') as username from Member m";
